ce9a8ca3d53c766f76e131d1f60680b4175cbbf1
Yanni <yannidilly.prog@gmail.com>
2022-09-21 15:49:13 -0300

    requisito 2, criação e impĺementação do componente SolarSystem

---
+++ src/components/SolarSystem.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class SolarSystem extends React.Component {
+  render() {
+    return (
+      <div data-testid="solar-system">
+        <h2>teste</h2>
+      </div>
+    );
+  }
+}
+
+export default SolarSystem;
