f629d05e8eb35e32bd57e0d49aafe2999f03cac6
Yanni <yannidilly.prog@gmail.com>
2022-09-21 21:13:22 -0300

    requisito 10, implementação do componente MissionCard no array mission

--- src/components/MissionCard.js
+++ src/components/MissionCard.js
@@ -6,7 +6,7 @@ class MissionCard extends React.Component {
     const { name, year, country, destination } = this.props;
     return (
       <div data-testid="mission-card">
-        <p data-testid="mission-name">{ name }</p>
+        <h3 data-testid="mission-name">{ name }</h3>
         <p data-testid="mission-year">{ year }</p>
         <p data-testid="mission-country">{ country }</p>
         <p data-testid="mission-destination">{ destination }</p>
